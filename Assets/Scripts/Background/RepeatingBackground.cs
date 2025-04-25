bcffdeb43a6bad5ffdf8f2737ec053d85656a303
Alestln <itworkplace46@gmail.com>
2025-04-25 17:25:53 +0300

    plus step

--- Assets/Scripts/Background/RepeatingBackground.cs
+++ Assets/Scripts/Background/RepeatingBackground.cs
@@ -5,13 +5,14 @@ using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 public class RepeatingBackground : MonoBehaviour
 {
     [SerializeField] private Camera cam;
-    [Range(0f, 0.5f)][SerializeField] private float offsetPercent = 0.05f;
     [Range(0f, 1f)][SerializeField] private float parallaxFactor = 0.5f;
 
     private float spriteWidth;
     private Transform leftBg;
     private Transform centerBg;
     private Transform rightBg;
+
+    private bool _leftCrossed, _rightCrossed;
 
     private Vector3 prevCamPos;
     private Vector3 parallaxOffset;
@@ -50,34 +51,36 @@ public class RepeatingBackground : MonoBehaviour
         rightBg.position = centerBg.position + new Vector3(spriteWidth, 0f, 0f);
 
         float camHalfWidth = cam.orthographicSize * cam.aspect;
-        float offset = camHalfWidth * offsetPercent;
 
         float cameraLeftEdge = cam.transform.position.x - camHalfWidth;
         float cameraRightEdge = cam.transform.position.x + camHalfWidth;
 
-        float rightmostBgEdge = rightBg.position.x + spriteWidth / 2f;
-        float leftmostBgEdge = leftBg.position.x - spriteWidth / 2f;
+        float rightmostBgEdge = centerBg.position.x + spriteWidth / 2f;
+        float leftmostBgEdge = centerBg.position.x - spriteWidth / 2f;
 
-        // Камера ушла вправо — перемещаем левый фон за правый
-        if (cameraRightEdge + offset > rightmostBgEdge)
+        if (cameraLeftEdge > rightmostBgEdge)
         {
             leftBg.position = rightBg.position + Vector3.right * spriteWidth;
-            SwapBackgrounds();
+
+            Transform centerTemp = centerBg;
+            Transform leftTemp = leftBg;
+            Transform rightTemp = rightBg;
+
+            centerBg = leftTemp;
+            rightBg = centerTemp;
+            leftBg = rightTemp;
         }
-        // Камера ушла влево — перемещаем правый фон за левый
-        else if (cameraLeftEdge - offset < leftmostBgEdge)
+        else if (cameraRightEdge < leftmostBgEdge)
         {
             rightBg.position = leftBg.position - Vector3.right * spriteWidth;
-            SwapBackgrounds();
-        }
-    }
 
-    private void SwapBackgrounds()
-    {
-        // Меняем порядок фонов
-        Transform temp = leftBg;
-        leftBg = centerBg;
-        centerBg = rightBg;
-        rightBg = temp;
+            Transform centerTemp = centerBg;
+            Transform leftTemp = leftBg;
+            Transform rightTemp = rightBg;
+
+            centerBg = rightTemp;
+            leftBg = centerTemp;
+            rightBg = leftTemp;
+        }
     }
 }
